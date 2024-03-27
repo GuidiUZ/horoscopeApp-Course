ab17a44ebfb9e3031990bbca23d1ac34d9c12b41
Guido <guiara_1998@hotmail.com>
2024-03-27 15:37:58 -0300

    add detail screen

---
+++ app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -0,0 +1,29 @@
+package com.example.horoscapp.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.navigation.navArgs
+import com.example.horoscapp.databinding.ActivityHoroscopeDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class HoroscopeDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHoroscopeDetailBinding
+    private val horoscopeDetailViewModel by viewModels<HoroscopeDetailViewModel>()
+
+    private val args : HoroscopeDetailActivityArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUi()
+    }
+
+    private fun initUi() {
+
+    }
+}
