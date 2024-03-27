b633d735e05c4502983c90f14e4b4d229fc63aa8
Guido <guiara_1998@hotmail.com>
2024-03-27 15:37:40 -0300

    add detail screen

--- app/src/main/java/com/example/horoscapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
+++ app/src/main/java/com/example/horoscapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -1,18 +1,35 @@
 package com.example.horoscapp.ui.horoscope.adapter
 
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.example.horoscapp.databinding.ItemHoroscopeBinding
 import com.example.horoscapp.domain.model.HoroscopeInfo
 
-class HoroscopeViewHolder(view: View) : RecyclerView.ViewHolder(view){
+class HoroscopeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = ItemHoroscopeBinding.bind(view)
 
-    fun render(horoscopeInfo: HoroscopeInfo) {
+    fun render(horoscopeInfo: HoroscopeInfo, onItemSelected: (HoroscopeInfo) -> Unit) {
         val context = binding.tvHoroscope.context
         binding.ivHoroscope.setImageResource(horoscopeInfo.image)
         binding.tvHoroscope.text = context.getString(horoscopeInfo.name)
+        binding.itemParent.setOnClickListener {
+            startRotation(binding.ivHoroscope, newLambda = {onItemSelected(horoscopeInfo)})
+        }
     }
+
+    private fun startRotation(view: View, newLambda : () -> Unit) {
+        view.animate().apply {
+            duration = 500
+            interpolator = LinearInterpolator()
+            rotationBy(360f)
+            withEndAction {
+                newLambda()
+            }
+            start()
+        }
+    }
+
 
 }
